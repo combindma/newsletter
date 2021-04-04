a5d800106070968b610991d0e0ea5dedcb9ae3e0
Omar Herri <herri.omar@gmail.com>
2021-04-04 16:28:46 +0100

    wip

--- src/Newsletter.php
+++ src/Newsletter.php
@@ -3,6 +3,7 @@
 namespace Combindma\Newsletter;
 
 use Combindma\Newsletter\Http\Controllers\NewsletterController;
+use Combindma\Newsletter\Models\NewsletterSubscription;
 use Illuminate\Support\Facades\Route;
 
 class Newsletter
@@ -13,5 +14,17 @@ class Newsletter
             Route::resource('newsletter', NewsletterController::class)->except(['show'])->parameters(['newsletter' => 'subscriber']);
             Route::post('/newsletter/{id}/restore', [NewsletterController::class, 'restore'])->name('newsletter.restore');
         });
+    }
+
+    public static function create(array $data)
+    {
+        NewsletterSubscription::create($data);
+        //TODO: Add mailchimp integration
+        /*$list_id = config('newsletter.listId');
+        $MailChimp = new MailChimp(config('newsletter.apiKey'));
+        $MailChimp->post("lists/$list_id/members", [
+            'email_address' => $request->input('email'),
+            'status'        => 'subscribed',
+        ]);*/
     }
 }
